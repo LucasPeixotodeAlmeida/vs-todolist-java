f340d8c10683883153abd06b1e7520a4e46edd94
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-04-24 20:38:23 -0300

    iniciando repository

--- todolist/src/main/java/br/com/lucaspeixoto/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/lucaspeixoto/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package br.com.lucaspeixoto.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data //lombok cria automaticamente os getters and setters usando o @Data. Caso necessário ter apenas o getter ou setter em um atributo basta fazer a anotação em cima do atributo
+@Entity(name="tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
